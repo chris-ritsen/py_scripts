2c6da45042d04e8496f3c4c592a3f287b71d4f8a
Christopher Ritsen <chris.ritsen@gmail.com>
2016-06-10 15:19:34 -0400

    updated everything blindly.

--- tmux.py
+++ tmux.py
@@ -1,20 +1,73 @@
 
-from os import environ
-from subprocess import check_output
+import subprocess
+import os
 
-# TODO: I guess class stuff?
+class Tmux(object):
+  def __init__(self):
+    self.socket = None
+    self.sessions = []
 
-tmux_socket = environ["HOME"] + "/.tmp/tmux/tmux-1000/books"
+  def command(self, cmd):
+    cmd = [
+      "/usr/local/bin/tmux",
+      "-S",
+      self.socket
+    ] + cmd
+
+    subprocess.call(cmd)
+
+  def start_server(self):
+    for session in self.sessions:
+      windows = session["windows"]
+      self.new_session(session, windows[0])
+
+      for window in windows[1:]:
+        self.new_window(window)
+
+  def new_session(self, session, window):
+    cmd = [
+      "new-session",
+      "-d",
+      "-s", session["name"],
+      "-c", window["dir"],
+      "-n", window["name"],
+      window["command"]
+    ]
+
+    self.command(cmd)
+
+  def new_window(self, window):
+    cmd = [
+      "new-window",
+      "-t", window["target"],
+      "-c", window["dir"],
+      "-n", window["name"],
+      window["command"]
+    ]
+
+    self.command(cmd)
+
+  def has_server(self):
+    # if stat.S_ISSOCK(mode):
+    #   print("Socket file exists")
+
+    try:
+      subprocess.check_output([
+        "/usr/local/bin/tmux",
+        "-S",
+        self.socket,
+        "list-sessions",
+      ], stderr=subprocess.PIPE)
+      return True
+    except:
+      return False
+
+tmux_socket = os.environ["HOME"] + "/.tmp/tmux/tmux-1000/books"
 
 def cmd(command):
-  return check_output(command, shell=True).decode()
+  return subprocess.check_output(command, shell=True).decode()
 
 def attached():
   string = "tmux -S " + tmux_socket + " ls -F '#{session_attached}' | sort -ur | head -n1"
   return int(cmd(string)) > 0
-
-# def session_active():
-#   call(["tmux", "-S", tmux_socket, "has-session", "-t", "books"])
-#   string = "tmux -S " + tmux_socket + " ls -F '#{session_attached}' | sort -ur | head -n1"
-#   return int(cmd(string)) > 0
 
